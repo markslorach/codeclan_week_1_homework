2f9e8cefd8ef1d17aaeca3cdd3ea270d7d1ccea2
Mark Slorach <hello@markslorach.com>
2023-05-20 08:14:24 +0100

    5 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -11,4 +11,7 @@ def get_total_cash(pet_shop):
 # test 3 and 4
 def add_or_remove_cash(pet_shop, total_cash):
     pet_shop["admin"]["total_cash"] += total_cash
-    
+
+# test 5
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
