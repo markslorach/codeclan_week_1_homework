71a88af00e9085b1391f006ca3ba9ea84b6b623d
Mark Slorach <hello@markslorach.com>
2023-05-20 08:04:56 +0100

    3 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,10 @@
+import pdb
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, total_cash):
+    pet_shop["admin"]["total_cash"] += total_cash
