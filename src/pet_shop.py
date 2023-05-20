88c5ccea9d4c36c55b22c488ec8611c3cfb3f79b
Mark Slorach <hello@markslorach.com>
2023-05-20 10:09:16 +0100

    10 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -33,3 +33,9 @@ def get_pets_by_breed(pet_shop, pet_name):
 
     return pets
 
+# test 10
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+        
